fcc32e9cb598c2264db869491d7c81dd663c9609
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-09-02 14:34:15 +0400

    Do AutoMapper For Classes

---
+++ Profiles/MappingProfile.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using E_CommerceSystem.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        // -----------------
+        // Category 
+        // -----------------
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<CategoryCreateDto, Category>();
+        CreateMap<CategoryUpdateDto, Category>();
+
+        // -----------------
+        // Supplier 
+        // -----------------
+        CreateMap<Supplier, SupplierDTO>().ReverseMap();
+        CreateMap<SupplierCreateDto, Supplier>();
+        CreateMap<SupplierUpdateDto, Supplier>();
+
+        // -----------------
+        // Product 
+        // -----------------
+        CreateMap<Product, ProductDTO>().ReverseMap();
+
+        // -----------------
+        // User 
+        // -----------------
+        CreateMap<User, UserDTO>().ReverseMap();
+
+        // -----------------
+        // Order 
+        // -----------------
+        CreateMap<Order, OrdersOutputOTD>().ReverseMap();
+
+        // -----------------
+        // OrderProduct 
+        // -----------------
+        CreateMap<OrderProducts, OrderItemDTO>().ReverseMap();
+
+        // -----------------
+        // Review 
+        // -----------------
+        CreateMap<Review, ReviewDTO>().ReverseMap();
+    }
+}
