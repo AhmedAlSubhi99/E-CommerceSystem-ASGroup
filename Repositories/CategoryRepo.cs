01e5352235a37ab7743f2bd942355c13fa56baaa
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-09-02 14:32:19 +0400

    Create Repos For Category and Supplier

---
+++ Repositories/CategoryRepo.cs
@@ -0,0 +1,39 @@
+﻿using E_CommerceSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSystem.Repositories
+{
+    public class CategoryRepo : ICategoryRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepo(ApplicationDbContext context) => _context = context;
+
+        public async Task<IEnumerable<Category>> GetAllAsync() =>
+            await _context.Categories.ToListAsync();
+
+        public async Task<Category?> GetByIdAsync(int id) =>
+            await _context.Categories.FindAsync(id);
+
+        public async Task AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Category category)
+        {
+            _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _context.Categories.FindAsync(id);
+            if (entity != null)
+            {
+                _context.Categories.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
