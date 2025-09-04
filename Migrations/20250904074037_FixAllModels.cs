7e4c6da0aff3b419f574c1b8daea7f763526244b
Ahmed AlSubhi <cheer.codeline@gmail.com>
2025-09-04 11:40:56 +0400

    Update database

---
+++ Migrations/20250904074037_FixAllModels.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAllModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UID",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "UID",
+                table: "Orders",
+                newName: "UserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UID",
+                table: "Orders",
+                newName: "IX_Orders_UserId");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "UnitPrice",
+                table: "OrderProducts",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Users_UserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UnitPrice",
+                table: "OrderProducts");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Orders",
+                newName: "UID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Orders_UserId",
+                table: "Orders",
+                newName: "IX_Orders_UID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Users_UID",
+                table: "Orders",
+                column: "UID",
+                principalTable: "Users",
+                principalColumn: "UID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
